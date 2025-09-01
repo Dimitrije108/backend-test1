583ada006da18272eb9137f2e5ab90ca5b4ce3cc
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2025-09-01 15:23:42 +0200

    feat: implement users controller with get all user and create user functionality

---
+++ src/controllers/users.js
@@ -0,0 +1,56 @@
+const { PrismaClient } = require("../../generated/prisma");
+const prisma = new PrismaClient();
+const asyncHandler = require("express-async-handler");
+
+// TODO:
+// - Add validation
+// - Add other CRUD methods
+
+const getAllUsers = asyncHandler(
+	async (req, res) => {
+		const users = await prisma.user.findMany({ 
+			include: { 
+				inviter: true, 
+				invitees: true 
+			}
+		});
+
+		res.status(200).json(users);
+	}
+);
+
+const createUser = asyncHandler(
+	async (req, res) => {
+		const { name, invitedById } = req.body || {};
+		// Basic validation
+		if (!name) {
+			return res.status(400).json("Name is required");
+		};
+
+		if (invitedById) {
+			const inviter = await prisma.user.findUnique({ 
+				where: { 
+					id: invitedById 
+				} 
+			});
+			// Check if inviter exists
+			if (!inviter) {
+				return res.status(400).json("Inviter user does not exist");
+			};
+		};
+
+		const user = await prisma.user.create({ 
+			data: { 
+				name, 
+				inviterId: invitedById ?? null 
+			} 
+		});
+
+		res.status(201).json(user);
+	}
+);
+
+module.exports = {
+	getAllUsers,
+	createUser,
+};
